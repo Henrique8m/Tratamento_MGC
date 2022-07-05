0561ea347acc8ed8891bbbe22a4d56777f5fa2be
Robson Elias Barreto <hyck.rocha@gmail.com>
2022-07-04 21:54:46 -0300

    Updatede class SerialProperties and ManipulateFiles

--- src/main/java/com/hrodriguesdev/files/ManipulateFiles.java
+++ src/main/java/com/hrodriguesdev/files/ManipulateFiles.java
@@ -1,5 +1,46 @@
 package com.hrodriguesdev.files;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class ManipulateFiles {
+	
+	public static void writeFile(File arquivoProperties, List<String> configDefalt) {
+		try (BufferedWriter bw1 = new BufferedWriter(new FileWriter(arquivoProperties))){
+			for(String x : configDefalt) {
+				bw1.write(x);
+				bw1.newLine();
+			}
+		}
+		catch (IOException e ) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void createdDiretorioAndFile(List<String> configDefalt, String dirYggDrasil, String dirResource, String nameFileProperties) {
+		
+		try {
+			File diretorio1 = new File(System.getProperty("user.home").toString() + dirYggDrasil);
+			diretorio1.mkdir();
+			File diretorio2 = new File(System.getProperty("user.home").toString() +dirResource);
+			diretorio2.mkdir();
+			File arquivoProperties = new File(diretorio2, nameFileProperties );
+			if(!arquivoProperties.exists()) {
+				arquivoProperties.createNewFile(); 
+				writeFile(arquivoProperties, configDefalt);
+			}
+				
+			
+		}catch (IOException e1) {
+			e1.printStackTrace(); 
+		}catch (Exception e2) {
+			e2.printStackTrace();
+		}
+	
+	}
 
 }
--- src/main/java/com/hrodriguesdev/seriral/properties/SerialProperties.java
+++ src/main/java/com/hrodriguesdev/seriral/properties/SerialProperties.java
@@ -1,13 +1,12 @@
 package com.hrodriguesdev.seriral.properties;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.hrodriguesdev.files.ManipulateFiles;
 
 
 public class SerialProperties {
@@ -15,7 +14,50 @@ public class SerialProperties {
     private int baud = 0;
     private int timeout = 0;
     private int stopBits = 0;
+    private String paridade = "None";
 
+    public SerialProperties(String dirYggDrasil, String dirResource, String nameFileProperties){
+    	String pathToProperties;
+    	ManipulateFiles.createdDiretorioAndFile(configDefalt(), dirYggDrasil, dirResource, nameFileProperties);    	
+    	pathToProperties = System.getProperty("user.home").toString() + dirResource +"\\" + nameFileProperties;
+  
+    	if(pathToProperties!=null)
+    		try(BufferedReader br = new BufferedReader(new FileReader(pathToProperties) ) ){
+    			String itemsProperties = br.readLine();
+    			String[] line = new String[5];
+				int i = 0;
+    			while(itemsProperties != null) {				
+					line[i] = itemsProperties;								
+					itemsProperties = br.readLine();
+					i++;
+					
+				}	
+				if(!line[0].isEmpty())
+					baud =Integer.parseInt(line[0]);
+				if(!line[1].isEmpty())
+					stopBits =Integer.parseInt(line[1]);
+				if(!line[2].isEmpty())
+					porta =line[2] ;
+				
+	    	}catch (IOException e) {
+				System.out.println(e.getMessage());
+				
+			}catch(NullPointerException e) {
+				System.out.println("Falha no caminho do arquivo de propriedades!");
+				
+			}
+	}
+	
+	private List<String> configDefalt(){
+		List<String> list = new ArrayList<>();
+		list.add("9600");
+		list.add("1");
+		if(porta!=null) {
+			list.add(porta);
+		}
+		
+		return list;
+	}
 
 	public String getPorta() {
         return porta;
